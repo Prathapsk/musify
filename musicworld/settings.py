5ecf76bb4c7a3c1432e34e3aef1d5a638a621bbb
prathap <prathapsept1212@gmail.com>
2026-06-01 13:55:43 +0530

    fixed gmail ssl smtp

--- musicworld/settings.py
+++ musicworld/settings.py
@@ -164,15 +164,19 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_PORT = 587
+EMAIL_TIMEOUT = 30
 
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+
+EMAIL_USE_SSL = True
+
+EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-DEFAULT_FROM_EMAIL = 'prathapsept1212@gmail.com'
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
 
 # DEFAULT PRIMARY KEY
